572656e09a7bf77d686b331e26ff9fc25f1e7ed3
Oleksandr Solontsov <solontsov@gmail.com>
2023-04-17 12:22:57 +0100

    setGridPosition(element)

--- js/donkey.js
+++ js/donkey.js
@@ -45,13 +45,18 @@ class DonkeyBox {
         this.createVisualElement();
     }
 
+    setGridPosition(element) {
+        element.style.gridRow = `${this.row} / span ${this.height}`;
+        element.style.gridColumn = `${this.column} / span ${this.width}`;
+
+    }
+
     createVisualElement() {
         let element = document.createElement("div");
         element.innerHTML = this.name;
 
         element.className = "dnk-box";
-        element.style.gridRow = `${this.row} / span ${this.height}`;
-        element.style.gridColumn = `${this.column} / span ${this.width}`;
+        this.setGridPosition(element);
 
         // add click handlers
         // to make "this" available inside method handleClick 
@@ -71,6 +76,33 @@ class DonkeyBox {
         return true;
     }
 
+    //check if there is a space under the box
+    checkSpaceUnder() {
+        let row = this.row + 1; //row under
+        for (let column = this.column; column < this.column + this.width; column++) {
+            if (!donkeyGame.spaces.some(space => space.row === row && space.column === column)) return false;
+        }
+        return true;
+    }
+    //check if there is a space on the left of the box
+    checkSpaceLeft() {
+        let column = this.column - 1; // column on the left
+        for (let row = this.row; row < this.row + this.height; row++) {
+            if (!donkeyGame.spaces.some(space => space.row === row && space.column === column)) return false;
+        }
+        return true;
+    }
+
+    //check if there is a space on the right of the box
+    checkSpaceRight() {
+        let column = this.column + 1; // column on the right
+        for (let row = this.row; row < this.row + this.height; row++) {
+            if (!donkeyGame.spaces.some(space => space.row === row && space.column === column)) return false;
+        }
+        return true;
+    }
+
+
     //returns array of possible places, that are objects. For example, [ { row: 5, column: 2 }, {} ]
     // if there is no place to move, then returns [] (empty array)
     findPlacesToMove() {
@@ -78,7 +110,15 @@ class DonkeyBox {
 
         //check if there is a space above, under, on the left, on the right
         if (this.checkSpaceAbove()) result.push({ row: this.row - 1, column: this.column });
-        console.log("checkSpaceAbove", this.checkSpaceAbove())
+        if (this.checkSpaceUnder()) result.push({ row: this.row + 1, column: this.column });
+        if (this.checkSpaceLeft()) result.push({ row: this.row, column: this.column - 1 });
+        if (this.checkSpaceRight()) result.push({ row: this.row, column: this.column + 1 });
+
+        //TODO: debug
+        // console.log("checkSpaceAbove", this.checkSpaceAbove())
+        // console.log("checkSpaceUnder", this.checkSpaceUnder())
+        // console.log("checkSpaceLeft", this.checkSpaceLeft())
+        // console.log("checkSpaceRight", this.checkSpaceRight())
 
         return result;
     }
@@ -89,22 +129,35 @@ class DonkeyBox {
         let placesToMove = this.findPlacesToMove();
         if (placesToMove.length == 0) return;
 
+        let index = 0; // first element of placesToMove, if there is only one place
 
-        // for small boxes (1 by 1) there might be 2 direction to move 
-        let partClicked = 1;
+        if (placesToMove.length == 2) {
+            // 2 direction to move. By clicked part we need to determine direction
+            // TODO:
+            index = 1; //just to test
 
-        //TODO: handle clicks
-        console.log("CLICK", this.column)
-        console.log("CLICK", e.target)
+            // for small boxes (1 by 1) there might be  
+            let partClicked = 1;
+        }
+
+        // element in grid layout
+        let element = e.target;
 
         //put corresponding box with absolute position above the clicked one
+        //TODO: 
 
         //hide the clicked box (aligned to grid)
         //e.target.style.display = 'none';
 
+        //make changes in object model 
+        //deal with spaces
+        
+        Object.assign(this, placesToMove[index]);
+        
         //change the absolute position 
 
         //change grid position of the clicked box
+        this.setGridPosition(element);
 
         //show the clicked box after delay 
 
